15e789ff590dc25f1fb16b616affeea40aacecdb
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2024-10-30 19:06:44 -0300

    feat(deploy)
    Added ghpages and deploy to github pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,7 +9,7 @@ import TutorialsPage from "./pages/Tutorials";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<IndexPage />} />
+      <Route path="/portfolio-react" element={<IndexPage />} />
       <Route path="/form" element={<FormPage />} />
       <Route path="/projects" element={<ProjectsPage />} />
       <Route path="/github" element={<GithubPage />} />
